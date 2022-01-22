f7a60a773f08da22c42bd2f95c400709e58d1f17
Oleksandr Sydorenko <sydorenko867@gmail.com>
2022-01-22 13:09:30 +0200

    3rd point

--- main.js
+++ main.js
@@ -2,10 +2,18 @@
 
 const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
 
-const personalMovieDB = {
+let personalMovieDB = {
     count : numberOfFilms,
     movies : {},
     actors : {},
     genres : [],
     privat : false
 };
+
+const filmTitle = prompt("Один из последних просмотренных фильмов?", "");
+const filmRating = prompt("На сколько оцените его?", "");
+personalMovieDB.movies = {
+    filmTitle : filmRating
+};
+
+console.log(`${filmTitle} ${filmRating}`);
