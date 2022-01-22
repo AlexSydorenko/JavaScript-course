c60349c5d6144a38c8cc92ef032289812b25b1a6
Oleksandr Sydorenko <sydorenko867@gmail.com>
2022-01-22 13:00:54 +0200

    2nd point

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
 "use strict";
 
 const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
-console.log(numberOfFilms);
+
+const personalMovieDB = {
+    count : numberOfFilms,
+    movies : {},
+    actors : {},
+    genres : [],
+    privat : false
+};
